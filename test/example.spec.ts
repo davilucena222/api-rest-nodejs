c67ba5358ca5f8df6895ec9de5ca44e7c883164a
Davi Luiz <davilucena222@gmail.com>
2023-11-15 22:57:18 -0300

    finalizando a implementação de testes

--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest'
-
-test('O usuário consegue criar uma nova transação', () => {
-  // fazer a chamada HTTP para criar uma nova transação
-
-  const responseStatusCode = 201
-
-  expect(responseStatusCode).toEqual(201)
-})
